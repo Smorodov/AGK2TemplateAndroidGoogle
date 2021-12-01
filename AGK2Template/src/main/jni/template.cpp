c03ca518182e25999826b30d50646914c5d17d9e
Smorodov <andreysmorodov@gmail.com>
2021-12-01 19:25:22 +0300

    some fixes

--- AGK2Template/src/main/jni/template.cpp
+++ AGK2Template/src/main/jni/template.cpp
@@ -3,21 +3,27 @@
 
 // Namespace
 using namespace AGK;
-
+// https://steamcommunity.com/app/325180/discussions/0/141136086934977747/
 app App;
-
 void app::Begin(void)
 {
-	agk::SetVirtualResolution (1024, 768);
-    agk::SetClearColor( 151,170,204 ); // light blue
+	agk::SetFolder("/media");
+	int DeviceDPI=agk::GetDeviceDPI();
+	int DeviceHeight = agk::GetDeviceHeight();
+	int DeviceWidth = agk::GetDeviceWidth();
+
+	agk::SetVirtualResolution (agk::GetMaxDeviceWidth(), agk::GetMaxDeviceHeight());
+    agk::SetClearColor( 64,64,64 ); // light blue
     agk::SetSyncRate(60,0);
     agk::SetScissor(0,0,0,0);
+	agk::UseNewDefaultFonts(1);
 }
 
 int app::Loop (void)
 {
 	agk::Print( agk::ScreenFPS() );
-	agk::Print( agk::ScreenFPS() );
+	agk::Print( agk::GetMaxDeviceWidth());
+	agk::Print( agk::GetMaxDeviceHeight());
 	agk::Sync();
 
 	return 0;
